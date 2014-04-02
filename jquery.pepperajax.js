4b7d9801b4941da3f8eb445209ebcb29144a8e88
arknotts <arknotts@gmail.com>
2014-04-02 15:30:21 -0400

    Update jquery.pepperajax.js

--- jquery.pepperajax.js
+++ jquery.pepperajax.js
@@ -4,154 +4,165 @@
         return this.filter(selector).add(this.find(selector));
     };
 
-    $.fn.pepperAjax = function (options) {
-        var $thisElement = $(this);
+    var settings;
 
-        var settings = $.extend({
-            onAjaxError: function () { alert('We were unable to process your request at this time.'); },
-            json: false,
-            jsonHtmlKey: 'html'
-        }, options);
+    function prependInputIndexers(content) {
+        //add the list-item indexers on form submit
+        var allListItems = content.find('[data-behavior=list-item]');
 
-        function initContent(content) {
+        while (allListItems.length > 0) {
+            var first = allListItems.first();
+            var oneSetOfListItems = first.add(first.siblings('[data-behavior=list-item]'));
+
+            oneSetOfListItems.each(function (listItemIdx) {
+                var $thisListItem = $(this);
+
+                var inputs = $thisListItem.find('input, select, textarea');
+                inputs.each(function () {
+                    var $thisInput = $(this);
+                    if ($thisInput.attr('name').indexOf('[') != 0) { //don't append multiple times
+                        $thisInput.attr('name', '[' + listItemIdx + '].' + $thisInput.attr('name'));
+                    }
+                });
+            });
+
+            //remove ones that are already done
+            allListItems = allListItems.not(oneSetOfListItems);
+        }
+    }
+
+    function doAjax(ajaxContent, silent) {
+
+        //pull the data attributes of the element (generally data values take precedence over regular action and method attributes)
+        var resultDestination = ajaxContent.data('result-destination') || ajaxContent; //default to itself
+        var resultBehavior = ajaxContent.data('result-behavior') || 'replace';
+        var additionalForms = ajaxContent.data('forms');
+        var ajaxMethod = ajaxContent.data('method') || ajaxContent.attr('method') || 'get';
+        var url = ajaxContent.data('url') || ajaxContent.attr('action');
+        var refreshOnAjax = ajaxContent.data('refresh-onajax');
+
+        //gather the data to send
+        //find where we need to serialize from...use a boundary if it's found in the parent chain, otherwise itself
+        var boundary = ajaxContent.data('boundary') != null ? ajaxContent : ajaxContent.parents('[data-boundary]');
+        var toSerializeRoot = boundary.length > 0 ? boundary : ajaxContent;
+        var requestData = ajaxContent.add(toSerializeRoot.find('form,input,select,textarea')).add(additionalForms).serialize();
+
+        //make the ajax call
+        $.ajax({
+            url: url,
+            type: ajaxMethod,
+            data: requestData,
+            statusCode: {
+                500: function () { settings.onAjaxError(); }
+            }
+        }).done(function (data) {
+
+            var html = settings.json ? data[settings.jsonHtmlKey] : data;
+            html = $(html);
+
+            var refreshOnLoad = html.data('refresh-onload');
+
+            function showHtml() {
+                html.fadeIn(function () {
+                    //focus the first non-hidden input
+                    $(this).find('input').not('[type=hidden]').first().focus();
+                });
+            }
+
+            var eventTarget = html; //target to trigger ajaxLoaded event on
+            html.hide();
+
+            var resultDestinationElement;
+            if (resultDestination == 'parent-boundary') {
+                resultDestinationElement = ajaxContent.parents('[data-boundary]');
+            } else {
+                resultDestinationElement = $(resultDestination);
+            }
+
+            if (resultBehavior == 'append') {
+                resultDestinationElement.append(html);
+                showHtml();
+                eventTarget.trigger('ajaxLoaded', [html]);
+            }
+            else if (resultBehavior == 'replace') {
+                var oldHeight = resultDestinationElement.height();
+
+                if (html == null || html.length == 0) {
+                    //special case if the result destination will be replaced with nothing
+                    //(i.e. a delete), we will have no element to trigger ajaxLoaded on.
+                    //So we just use the next best choice: parent of the destination
+                    eventTarget = resultDestinationElement.parent();
+
+                    resultDestinationElement.fadeOut(function () {
+                        resultDestinationElement.remove();
+                        eventTarget.trigger('ajaxLoaded', [html]);
+                    });
+                }
+                else {
+                    if (silent) {
+                        //silent...just do a simple replace, no fancy opacity or height manipulations
+                        html.show();
+                        resultDestinationElement.replaceWith(html);
+                    }
+                    else {
+                        $('body').height($('body').height() + 1000); //helps smooth out dom manipulations at the bottom of the page
+
+                        //add the new content to the dom (still hidden)
+                        resultDestinationElement.replaceWith(html);
+
+                        //get its height
+                        var newHeight = html.height();
+                        //set its height to the old value to start animation
+                        html.height(oldHeight);
+                        //show it
+                        showHtml();
+                        //animate to new height
+                        html.height(newHeight);
+                        //set height to auto so things don't get hidden with the overflow: hidden
+                        setTimeout(function () { html.css('height', ''); }, 500);
+
+                        $('body').height('auto'); //reset the height that was fiddled with at the top
+                    }
+
+                    eventTarget.trigger('ajaxLoaded', [html]);
+                }
+            }
+
+            $(refreshOnAjax).add(refreshOnLoad).pepperAjax('refresh', {silent: true});
+        });
+    }
+
+    var methods = {
+        init: function (content, options) {
+            
+            settings = $.extend({
+                onAjaxError: function () { alert('We were unable to process your request at this time.'); },
+                json: false,
+                jsonHtmlKey: 'html',
+                prependInputIndexers: false
+            }, options);
+
             content = content || $('body'); //default to everything
 
             content.thisAndChildren('[data-behavior=ajax]').each(function () {
                 var $this = $(this);
-                var resultDestination = $this.data('result-destination') || $this; //default to itself
-                var resultBehavior = $this.data('result-behavior') || 'replace';
-                var additionalForms = $($this.data('forms'));
-                var ajaxMethod = 'get';
-                var url;
-
-                function callback(e) {
-                    e.preventDefault();
-
-                    //gather the data
-                    var requestData = $this //include itself
-                               .add($this.parents('form')) //include its parent form
-                               .add(additionalForms) //include any additional forms listed
-                               .serialize(); //serialize them all together
-
-                    //make the ajax call
-                    $.ajax({
-                        url: url,
-                        type: ajaxMethod,
-                        data: requestData,
-                        statusCode: {
-                            500: function () { settings.onAjaxError(); }
-                        }
-                    }).done(function (data) {
-
-                        var html = settings.json ? data[settings.jsonHtmlKey] : data;
-                        html = $(html);
-
-                        function showHtml() {
-                            html.fadeIn(function () {
-                                //focus the first non-hidden input
-                                $(this).find('input').not('[type=hidden]').first().focus();
-                            });
-                        }
-
-                        var eventTarget = html; //target to trigger ajaxLoaded event on
-                        html.hide();
-
-                        var resultDestinationElement;
-                        if (resultDestination == 'parent-boundary') {
-                            resultDestinationElement = $this.parents('[data-behavior=boundary]');
-                        } else {
-                            resultDestinationElement = $(resultDestination);
-                        }
-
-                        if (resultBehavior == 'append') {
-                            resultDestinationElement.append(html);
-                            showHtml();
-                            eventTarget.trigger('ajaxLoaded', [html]);
-                        }
-                        else if (resultBehavior == 'replace') {
-                            var oldHeight = resultDestinationElement.height();
-
-                            if (html.length == 0) {
-                                //special case if the result destination will be replaced with nothing
-                                //(i.e. a delete), we will have no element to trigger ajaxLoaded on.
-                                //So we just use the next best choice: parent of the destination
-                                eventTarget = resultDestinationElement.parent();
-
-                                resultDestinationElement.fadeOut(function () {
-                                    resultDestinationElement.remove();
-                                    eventTarget.trigger('ajaxLoaded', [html]);
-                                });
-                            }
-                            else {
-                                $('body').height($('body').height() + 1000); //helps smooth out dom manipulations at the bottom of the page
-
-                                //add the new content to the dom (still hidden)
-                                resultDestinationElement.replaceWith(html);
-                                //get its height
-                                var newHeight = html.height();
-                                //set its height to the old value to start animation
-                                html.height(oldHeight);
-                                //show it
-                                showHtml();
-                                //animate to new height
-                                html.height(newHeight);
-                                //set height to auto so things don't get hidden with the overflow: hidden
-                                setTimeout(function () { html.css('height', ''); }, 500);
-
-                                $('body').height('auto'); //reset the height that was fiddled with at the top
-
-                                eventTarget.trigger('ajaxLoaded', [html]);
-                            }
-                        }
-
-
-                    });
-                }
-
-                //data-method overrides the default
-                if ($this.data('method') != null) {
-                    ajaxMethod = $this.data('method');
-                }
 
                 if ($this.prop("tagName") == "FORM") {
-                    url = $this.attr('action');
-                    if ($this.attr('method') != null) {
-                        ajaxMethod = $this.attr('method');
-                    }
-
                     $this.on('submit', function (e) {
-                        var $thisForm = $(this);
+                        e.preventDefault();
 
-                        //add the list-item indexers on form submit
-                        var allListItems = $thisForm.find('[data-behavior=list-item]');
-
-                        while (allListItems.length > 0) {
-                            var first = allListItems.first();
-                            var oneSetOfListItems = first.add(first.siblings('[data-behavior=list-item]'));
-
-                            oneSetOfListItems.each(function (listItemIdx) {
-                                var $thisListItem = $(this);
-
-                                var inputs = $thisListItem.find('input, select, textarea');
-                                inputs.each(function () {
-                                    var $thisInput = $(this);
-                                    if ($thisInput.attr('name').indexOf('[') != 0) { //don't append multiple times
-                                        $thisInput.attr('name', '[' + listItemIdx + '].' + $thisInput.attr('name'));
-                                    }
-                                });
-                            });
-
-                            //remove ones that are already done
-                            allListItems = allListItems.not(oneSetOfListItems);
+                        if (settings.prependInputIndexers) {
+                            prependInputIndexers($this);
                         }
-
-                        callback(e);
+                        
+                        doAjax($this);
                     });
                 }
                 else {
-                    url = $this.attr('data-url');
-
-                    $this.on('click', callback);
+                    $this.on('click', function (e) {
+                        e.preventDefault();
+                        doAjax($this);
+                    });
                 }
             });
 
@@ -167,13 +178,31 @@
                     toRemove.fadeOut(function () { toRemove.remove(); });
                 });
             });
+        },
+        refresh: function (options) {
+            this.each(function () {
+                var doSilentRefresh = options && options.silent;
+                doAjax($(this), doSilentRefresh);
+            });
         }
+    };
 
-        initContent($thisElement);
+    $.fn.pepperAjax = function (methodOrOptions) {
+        var $thisElement = $(this);
+
+        if (methods[methodOrOptions]) {
+            //call a method if it was passed
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            //else default to init (assuming they also passed options)
+            methods['init']($thisElement, methodOrOptions);
+        } else {
+            $.error('Method ' + methodOrOptions + ' does not exist.');
+        }
 
         $thisElement.on('ajaxLoaded', function (e, newContent) {
             //initialize all of the ajax on new content
-            initContent(newContent);
+            methods['init'](newContent, methodOrOptions);
         });
     }
 }(jQuery));
